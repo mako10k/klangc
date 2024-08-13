5dd1ae6e07278f1445e4a2a6d981a8f32e5a867b
Makoto Katsumata <mako10k@mk10.org>
2024-08-13 13:18:57 +0900

    add comment for pat_as

--- src/pat_as.c
+++ src/pat_as.c
@@ -5,44 +5,66 @@
 #include "pat_ref.h"
 #include <assert.h>
 
+// *******************************
+// Pattern As.
+// *******************************
+// -------------------------------
+// Structures.
+// -------------------------------
+/**
+ * Pattern as.
+ */
 struct klangc_pat_as {
+  /** Reference */
   klangc_pat_ref_t *kpas_ref;
+  /** Pattern */
   klangc_pat_t *kpas_pat;
 };
 
-klangc_pat_as_t *klangc_pat_as_new(klangc_pat_ref_t *var, klangc_pat_t *pat) {
-  assert(var != NULL);
+// -------------------------------
+// Constructors.
+// -------------------------------
+klangc_pat_as_t *klangc_pat_as_new(klangc_pat_ref_t *pref, klangc_pat_t *pat) {
+  assert(pref != NULL);
   assert(pat != NULL);
-
   klangc_pat_as_t *as = klangc_malloc(sizeof(klangc_pat_as_t));
-  as->kpas_ref = var;
+  as->kpas_ref = pref;
   as->kpas_pat = pat;
   return as;
 }
 
-klangc_pat_ref_t *klangc_pat_as_get_ref(klangc_pat_as_t *as) {
-  assert(as != NULL);
-  return as->kpas_ref;
+// -------------------------------
+// Accessors.
+// -------------------------------
+klangc_pat_ref_t *klangc_pat_as_get_ref(klangc_pat_as_t *pas) {
+  assert(pas != NULL);
+  return pas->kpas_ref;
 }
 
-klangc_pat_t *klangc_pat_as_get_pat(klangc_pat_as_t *as) {
-  assert(as != NULL);
-  return as->kpas_pat;
+klangc_pat_t *klangc_pat_as_get_pat(klangc_pat_as_t *pas) {
+  assert(pas != NULL);
+  return pas->kpas_pat;
 }
 
+// -------------------------------
+// Printers.
+// -------------------------------
+void klangc_pat_as_print(klangc_output_t *output, klangc_pat_as_t *pas) {
+  assert(output != NULL);
+  assert(pas != NULL);
+  klangc_printf(output, "%s@",
+                klangc_pat_ref_get_name(klangc_pat_as_get_ref(pas)));
+  klangc_pat_print(output, KLANGC_PREC_LOWEST, klangc_pat_as_get_pat(pas));
+}
+
+// -------------------------------
+// Binders.
+// -------------------------------
 klangc_bind_result_t klangc_pat_as_bind(klangc_expr_env_t *env,
-                                        klangc_pat_as_t *pat_as,
+                                        klangc_pat_as_t *pas,
                                         klangc_expr_ref_target_t *target) {
-  klangc_bind_result_t res = klangc_pat_ref_bind(env, pat_as->kpas_ref, target);
+  klangc_bind_result_t res = klangc_pat_ref_bind(env, pas->kpas_ref, target);
   if (res != KLANGC_BIND_OK)
     return res;
-  return klangc_pat_bind(env, pat_as->kpas_pat, target);
-}
-
-void klangc_pat_as_print(klangc_output_t *output, klangc_pat_as_t *pat_as) {
-  assert(output != NULL);
-  assert(pat_as != NULL);
-  klangc_printf(output, "%s@",
-                klangc_pat_ref_get_name(klangc_pat_as_get_ref(pat_as)));
-  klangc_pat_print(output, KLANGC_PREC_LOWEST, klangc_pat_as_get_pat(pat_as));
+  return klangc_pat_bind(env, pas->kpas_pat, target);
 }
--- src/pat_as.h
+++ src/pat_as.h
@@ -2,14 +2,50 @@
 
 #include "klangc.h"
 
-klangc_pat_as_t *klangc_pat_as_new(klangc_pat_ref_t *var, klangc_pat_t *pat);
+// *******************************
+// Pattern As.
+// *******************************
+// -------------------------------
+// Constructors.
+// -------------------------------
+/**
+ * Make a new pattern as.
+ * @param pref Reference.
+ * @param pat Pattern.
+ * @return New pattern as.
+ */
+klangc_pat_as_t *klangc_pat_as_new(klangc_pat_ref_t *pref, klangc_pat_t *pat);
 
-klangc_pat_ref_t *klangc_pat_as_get_ref(klangc_pat_as_t *as);
+// -------------------------------
+// Accessors.
+// -------------------------------
+/**
+ * Get the reference of a pattern as.
+ * @param pas Pattern as.
+ * @return Reference.
+ */
+klangc_pat_ref_t *klangc_pat_as_get_ref(klangc_pat_as_t *pas);
 
-klangc_pat_t *klangc_pat_as_get_pat(klangc_pat_as_t *as);
+/**
+ * Get the pattern of a pattern as.
+ * @param pas Pattern as.
+ * @return Pattern.
+ */
+klangc_pat_t *klangc_pat_as_get_pat(klangc_pat_as_t *pas);
 
-void klangc_pat_as_print(klangc_output_t *output, klangc_pat_as_t *pat_as);
+// -------------------------------
+// Printers.
+// -------------------------------
+/**
+ * Print a pattern as.
+ * @param output Output.
+ * @param pas Pattern as.
+ */
+void klangc_pat_as_print(klangc_output_t *output, klangc_pat_as_t *pas);
 
+// -------------------------------
+// Binders.
+// -------------------------------
 klangc_bind_result_t klangc_pat_as_bind(klangc_expr_env_t *env,
-                                        klangc_pat_as_t *pat_as,
+                                        klangc_pat_as_t *pas,
                                         klangc_expr_ref_target_t *target);
