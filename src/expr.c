591d086a5ea9d54a456a574028fcae4d14231c22
Makoto Katsumata <mako10k@mk10.org>
2024-08-13 11:31:47 +0900

    add comment for expr

--- src/expr.c
+++ src/expr.c
@@ -12,25 +12,67 @@
 #include "types.h"
 #include <assert.h>
 
+// *******************************
+// Expression.
+// *******************************
+// -------------------------------
+// Structures.
+// -------------------------------
+/**
+ * Expression.
+ */
 struct klangc_expr {
+  /** Type */
   klangc_expr_type_t ke_type;
   union {
-    int ke_intval;
-    const char *ke_strval;
-    klangc_expr_ref_t *ke_ref;
-    klangc_expr_appl_t *ke_appl;
+    /** Algebraic expression */
     klangc_expr_alge_t *ke_alge;
+    /** Reference expression */
+    klangc_expr_ref_t *ke_ref;
+    /** Application expression */
+    klangc_expr_appl_t *ke_appl;
+    /** Integer value */
+    int ke_intval;
+    /** String value */
+    const char *ke_strval;
+    /** Lambda expression */
     klangc_expr_lambda_t *ke_lambda;
+    /** Closure expression */
     klangc_expr_closure_t *ke_closure;
   };
+  /** Input position */
   klangc_ipos_t ke_ipos;
 };
 
-klangc_expr_t *klangc_expr_new_ref(klangc_expr_ref_t *ref, klangc_ipos_t ipos) {
-  assert(ref != NULL);
+// -------------------------------
+// Constructors.
+// -------------------------------
+klangc_expr_t *klangc_expr_new_alge(klangc_expr_alge_t *ealge,
+                                    klangc_ipos_t ipos) {
+  assert(ealge != NULL);
+  klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
+  expr->ke_type = KLANGC_ETYPE_ALGE;
+  expr->ke_alge = ealge;
+  expr->ke_ipos = ipos;
+  return expr;
+}
+
+klangc_expr_t *klangc_expr_new_ref(klangc_expr_ref_t *eref,
+                                   klangc_ipos_t ipos) {
+  assert(eref != NULL);
   klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
   expr->ke_type = KLANGC_ETYPE_REF;
-  expr->ke_ref = ref;
+  expr->ke_ref = eref;
+  expr->ke_ipos = ipos;
+  return expr;
+}
+
+klangc_expr_t *klangc_expr_new_appl(klangc_expr_appl_t *eappl,
+                                    klangc_ipos_t ipos) {
+  assert(eappl != NULL);
+  klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
+  expr->ke_type = KLANGC_ETYPE_APPL;
+  expr->ke_appl = eappl;
   expr->ke_ipos = ipos;
   return expr;
 }
@@ -52,47 +94,93 @@ klangc_expr_t *klangc_expr_new_string(const char *strval, klangc_ipos_t ipos) {
   return expr;
 }
 
-klangc_expr_t *klangc_expr_new_lambda(klangc_expr_lambda_t *lambda,
+klangc_expr_t *klangc_expr_new_lambda(klangc_expr_lambda_t *elambda,
                                       klangc_ipos_t ipos) {
-  assert(lambda != NULL);
+  assert(elambda != NULL);
   klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
   expr->ke_type = KLANGC_ETYPE_LAMBDA;
-  expr->ke_lambda = lambda;
+  expr->ke_lambda = elambda;
   expr->ke_ipos = ipos;
   return expr;
 }
 
-klangc_expr_t *klangc_expr_new_alge(klangc_expr_alge_t *alge,
-                                    klangc_ipos_t ipos) {
-  assert(alge != NULL);
-  klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
-  expr->ke_type = KLANGC_ETYPE_ALGE;
-  expr->ke_alge = alge;
-  expr->ke_ipos = ipos;
-  return expr;
-}
-
-klangc_expr_t *klangc_expr_new_appl(klangc_expr_appl_t *appl,
-                                    klangc_ipos_t ipos) {
-  assert(appl != NULL);
-  klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
-  expr->ke_type = KLANGC_ETYPE_APPL;
-  expr->ke_appl = appl;
-  expr->ke_ipos = ipos;
-  return expr;
-}
-
-klangc_expr_t *klangc_expr_new_closure(klangc_expr_closure_t *closure,
+klangc_expr_t *klangc_expr_new_closure(klangc_expr_closure_t *eclosure,
                                        klangc_ipos_t ipos) {
   klangc_expr_t *expr = klangc_malloc(sizeof(klangc_expr_t));
   expr->ke_type = KLANGC_ETYPE_CLOSURE;
-  expr->ke_closure = closure;
+  expr->ke_closure = eclosure;
   expr->ke_ipos = ipos;
   return expr;
 }
 
+// -------------------------------
+// Accessors.
+// -------------------------------
+klangc_expr_type_t klangc_expr_get_type(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  return expr->ke_type;
+}
+
+klangc_expr_alge_t *klangc_expr_get_alge(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_ALGE);
+  return expr->ke_alge;
+}
+
+klangc_expr_ref_t *klangc_expr_get_ref(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_REF);
+  return expr->ke_ref;
+}
+
+klangc_expr_appl_t *klangc_expr_get_appl(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_APPL);
+  return expr->ke_appl;
+}
+
+int klangc_expr_get_int(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_INT);
+  return expr->ke_intval;
+}
+
+const char *klangc_expr_get_string(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_STRING);
+  return expr->ke_strval;
+}
+
+klangc_ipos_t klangc_expr_get_ipos(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  return expr->ke_ipos;
+}
+
+klangc_expr_lambda_t *klangc_expr_get_lambda(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_LAMBDA);
+  return expr->ke_lambda;
+}
+
+klangc_expr_closure_t *klangc_expr_get_closure(klangc_expr_t *expr) {
+  assert(expr != NULL);
+  assert(expr->ke_type == KLANGC_ETYPE_CLOSURE);
+  return expr->ke_closure;
+}
+
+// -------------------------------
+// Parsers.
+// -------------------------------
+/**
+ * Parse a parenthesized expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
 static klangc_parse_result_t klangc_expr_parse_paren(klangc_input_t *input,
                                                      klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
   klangc_ipos_t ipos = klangc_input_save(input);
   klangc_ipos_t ipos_ss = klangc_skipspaces(input);
   if (!klangc_expect(input, '(', NULL)) {
@@ -124,8 +212,153 @@ static klangc_parse_result_t klangc_expr_parse_paren(klangc_input_t *input,
   return KLANGC_PARSE_OK;
 }
 
-klangc_parse_result_t klangc_expr_parse_closure(klangc_input_t *input,
-                                                klangc_expr_t **pexpr) {
+/**
+ * Parse an algebraic expression.
+ * @param input input
+ * @param epopt expression parse option
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t
+klangc_expr_parse_alge(klangc_input_t *input, klangc_expr_parse_opt_t epopt,
+                       klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_ipos_t ipos = klangc_input_save(input);
+  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
+  klangc_expr_alge_t *alge;
+  klangc_parse_result_t res;
+  res = klangc_expr_alge_parse(input, epopt, &alge);
+  if (res != KLANGC_PARSE_OK) {
+    klangc_input_restore(input, ipos);
+    return res;
+  }
+  *pexpr = klangc_expr_new_alge(alge, ipos_ss);
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse a reference expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t klangc_expr_parse_ref(klangc_input_t *input,
+                                                   klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_ipos_t ipos = klangc_input_save(input);
+  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
+  klangc_expr_ref_t *ref;
+  klangc_parse_result_t res;
+  res = klangc_expr_ref_parse(input, &ref);
+  if (res != KLANGC_PARSE_OK) {
+    klangc_input_restore(input, ipos);
+    return res;
+  }
+  *pexpr = klangc_expr_new_ref(ref, ipos_ss);
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse an application expression.
+ * @param input input
+ * @param epopt expression parse option
+ * @param efunc preparsed function expression
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t
+klangc_expr_parse_appl(klangc_input_t *input, klangc_expr_parse_opt_t epopt,
+                       klangc_expr_t *efunc, klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_expr_appl_t *appl;
+  klangc_parse_result_t res;
+  res = klangc_expr_appl_parse(input, epopt, efunc, &appl);
+  if (res != KLANGC_PARSE_OK)
+    return res;
+  *pexpr = klangc_expr_new_appl(appl, klangc_expr_get_ipos(efunc));
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse an integer expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t klangc_expr_parse_int(klangc_input_t *input,
+                                                   klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_ipos_t ipos = klangc_input_save(input);
+  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
+  int intval;
+  klangc_parse_result_t res = klangc_int_parse(input, &intval);
+  if (res != KLANGC_PARSE_OK) {
+    klangc_input_restore(input, ipos);
+    return KLANGC_PARSE_NOPARSE;
+  }
+  *pexpr = klangc_expr_new_int(intval, ipos_ss);
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse a string expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t klangc_expr_parse_string(klangc_input_t *input,
+                                                      klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_ipos_t ipos = klangc_input_save(input);
+  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
+  const char *strval;
+  klangc_parse_result_t res = klangc_string_parse(input, &strval);
+  if (res != KLANGC_PARSE_OK) {
+    klangc_input_restore(input, ipos);
+    return KLANGC_PARSE_NOPARSE;
+  }
+  *pexpr = klangc_expr_new_string(strval, ipos_ss);
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse a lambda expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t klangc_expr_parse_lambda(klangc_input_t *input,
+                                                      klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
+  klangc_ipos_t ipos = klangc_input_save(input);
+  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
+  klangc_expr_lambda_t *lambda;
+  klangc_parse_result_t res;
+  res = klangc_expr_lambda_parse(input, &lambda);
+  if (res != KLANGC_PARSE_OK) {
+    klangc_input_restore(input, ipos);
+    return res;
+  }
+  *pexpr = klangc_expr_new_lambda(lambda, ipos_ss);
+  return KLANGC_PARSE_OK;
+}
+
+/**
+ * Parse a closure expression.
+ * @param input input
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t klangc_expr_parse_closure(klangc_input_t *input,
+                                                       klangc_expr_t **pexpr) {
+  assert(input != NULL);
+  assert(pexpr != NULL);
   klangc_ipos_t ipos = klangc_input_save(input);
   klangc_ipos_t ipos_ss = klangc_skipspaces(input);
   klangc_expr_closure_t *closure;
@@ -143,97 +376,18 @@ klangc_parse_result_t klangc_expr_parse_closure(klangc_input_t *input,
   return KLANGC_PARSE_OK;
 }
 
-klangc_parse_result_t klangc_expr_parse_alge(klangc_input_t *input,
-                                             klangc_expr_parse_opt_t epopt,
-                                             klangc_expr_t **pexpr) {
-  klangc_ipos_t ipos = klangc_input_save(input);
-  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
-  klangc_expr_alge_t *alge;
-  klangc_parse_result_t res;
-  res = klangc_expr_alge_parse(input, epopt, &alge);
-  if (res != KLANGC_PARSE_OK) {
-    klangc_input_restore(input, ipos);
-    return res;
-  }
-  *pexpr = klangc_expr_new_alge(alge, ipos_ss);
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_appl(klangc_input_t *input,
-                                             klangc_expr_parse_opt_t epopt,
-                                             klangc_expr_t *efunc,
-                                             klangc_expr_t **pexpr) {
-  klangc_expr_appl_t *appl;
-  klangc_parse_result_t res;
-  res = klangc_expr_appl_parse(input, epopt, efunc, &appl);
-  if (res != KLANGC_PARSE_OK)
-    return res;
-  *pexpr = klangc_expr_new_appl(appl, klangc_expr_get_ipos(efunc));
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_ref(klangc_input_t *input,
-                                            klangc_expr_t **pexpr) {
-  klangc_ipos_t ipos = klangc_input_save(input);
-  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
-  klangc_expr_ref_t *ref;
-  klangc_parse_result_t res;
-  res = klangc_expr_ref_parse(input, &ref);
-  if (res != KLANGC_PARSE_OK) {
-    klangc_input_restore(input, ipos);
-    return res;
-  }
-  *pexpr = klangc_expr_new_ref(ref, ipos_ss);
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_int(klangc_input_t *input,
-                                            klangc_expr_t **pexpr) {
-  klangc_ipos_t ipos = klangc_input_save(input);
-  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
-  int intval;
-  klangc_parse_result_t res = klangc_int_parse(input, &intval);
-  if (res != KLANGC_PARSE_OK) {
-    klangc_input_restore(input, ipos);
-    return KLANGC_PARSE_NOPARSE;
-  }
-  *pexpr = klangc_expr_new_int(intval, ipos_ss);
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_string(klangc_input_t *input,
-                                               klangc_expr_t **pexpr) {
-  klangc_ipos_t ipos = klangc_input_save(input);
-  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
-  const char *strval;
-  klangc_parse_result_t res = klangc_string_parse(input, &strval);
-  if (res != KLANGC_PARSE_OK) {
-    klangc_input_restore(input, ipos);
-    return KLANGC_PARSE_NOPARSE;
-  }
-  *pexpr = klangc_expr_new_string(strval, ipos_ss);
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_lambda(klangc_input_t *input,
-                                               klangc_expr_t **pexpr) {
-  klangc_ipos_t ipos = klangc_input_save(input);
-  klangc_ipos_t ipos_ss = klangc_skipspaces(input);
-  klangc_expr_lambda_t *lambda;
-  klangc_parse_result_t res;
-  res = klangc_expr_lambda_parse(input, &lambda);
-  if (res != KLANGC_PARSE_OK) {
-    klangc_input_restore(input, ipos);
-    return res;
-  }
-  *pexpr = klangc_expr_new_lambda(lambda, ipos_ss);
-  return KLANGC_PARSE_OK;
-}
-
-klangc_parse_result_t klangc_expr_parse_noappl(klangc_input_t *input,
-                                               klangc_expr_parse_opt_t epopt,
-                                               klangc_expr_t **pexpr) {
+/**
+ * Parse an expression without application.
+ * @param input input
+ * @param epopt expression parse option
+ * @param pexpr expression
+ * @return parse result
+ */
+static klangc_parse_result_t
+klangc_expr_parse_noappl(klangc_input_t *input, klangc_expr_parse_opt_t epopt,
+                         klangc_expr_t **pexpr) {
   assert(input != NULL);
+  assert(pexpr != NULL);
   klangc_parse_result_t res;
   res = klangc_expr_parse_paren(input, pexpr);
   if (res != KLANGC_PARSE_NOPARSE)
@@ -296,6 +450,9 @@ klangc_parse_result_t klangc_expr_parse(klangc_input_t *input,
   return KLANGC_PARSE_OK;
 }
 
+// -------------------------------
+// Printers.
+// -------------------------------
 void klangc_expr_print(klangc_output_t *output, int prec, klangc_expr_t *expr) {
   assert(output != NULL);
   assert(expr != NULL);
@@ -324,6 +481,9 @@ void klangc_expr_print(klangc_output_t *output, int prec, klangc_expr_t *expr) {
   }
 }
 
+// -------------------------------
+// Binders.
+// -------------------------------
 klangc_bind_result_t klangc_expr_bind(klangc_expr_env_t *env,
                                       klangc_expr_t *expr) {
   switch (expr->ke_type) {
@@ -346,50 +506,4 @@ klangc_bind_result_t klangc_expr_bind(klangc_expr_env_t *env,
   }
   klangc_printf(kstderr, "Unknown expression type: %d\n", expr->ke_type);
   return KLANGC_BIND_ERROR;
-}
-
-klangc_ipos_t klangc_expr_get_ipos(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  return expr->ke_ipos;
-}
-
-klangc_expr_type_t klangc_expr_get_type(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  return expr->ke_type;
-}
-
-klangc_expr_ref_t *klangc_expr_get_ref(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_REF);
-  return expr->ke_ref;
-}
-
-klangc_expr_appl_t *klangc_expr_get_appl(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_APPL);
-  return expr->ke_appl;
-}
-
-int klangc_expr_get_int(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_INT);
-  return expr->ke_intval;
-}
-
-const char *klangc_expr_get_string(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_STRING);
-  return expr->ke_strval;
-}
-
-klangc_expr_lambda_t *klangc_expr_get_lambda(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_LAMBDA);
-  return expr->ke_lambda;
-}
-
-klangc_expr_closure_t *klangc_expr_get_closure(klangc_expr_t *expr) {
-  assert(expr != NULL);
-  assert(expr->ke_type == KLANGC_ETYPE_CLOSURE);
-  return expr->ke_closure;
 }
--- src/expr.h
+++ src/expr.h
@@ -3,108 +3,200 @@
 #include "klangc.h"
 #include "types.h"
 
-/** 式タイプ */
+// *******************************
+// Expressions.
+// *******************************
+// -------------------------------
+// Types.
+// -------------------------------
+/** Expression Type */
 typedef enum klangc_expr_type {
+  /** Algebratic Expression */
   KLANGC_ETYPE_ALGE,
+  /** Reference Expression */
   KLANGC_ETYPE_REF,
+  /** Application Expression */
   KLANGC_ETYPE_APPL,
+  /** Integer Expression */
   KLANGC_ETYPE_INT,
+  /** String Expression */
   KLANGC_ETYPE_STRING,
+  /** Lambda Expression */
   KLANGC_ETYPE_LAMBDA,
+  /** Closure Expression */
   KLANGC_ETYPE_CLOSURE,
 } klangc_expr_type_t;
 
-typedef enum {
+/** Parse options. */
+typedef enum klangc_expr_parse_opt {
+  /** parse normally */
   KLANGC_EXPR_PARSE_NORMAL = 0,
+  /** do not parse application */
   KLANGC_EXPR_PARSE_NOAPPL = 1,
 } klangc_expr_parse_opt_t;
 
+// -------------------------------
+// Constructors.
+// -------------------------------
 /**
- * 式をパースする
- * @param input 入力
- * @param pexpr パースした式
- * @return パース結果
+ * Create a new algebraic expression.
+ * @param ealge algebraic expression
+ * @param ipos position
+ * @return algebraic expression
+ */
+klangc_expr_t *klangc_expr_new_alge(klangc_expr_alge_t *ealge,
+                                    klangc_ipos_t ipos);
+
+/**
+ * Create a new reference expression.
+ * @param eref reference expression
+ * @param ipos position
+ * @return reference expression
+ */
+klangc_expr_t *klangc_expr_new_ref(klangc_expr_ref_t *eref, klangc_ipos_t ipos);
+
+/**
+ * Create a new application expression.
+ * @param eappl application expression
+ * @param ipos position
+ * @return application expression
+ */
+klangc_expr_t *klangc_expr_new_appl(klangc_expr_appl_t *eappl,
+                                    klangc_ipos_t ipos);
+
+/**
+ * Create a new integer expression.
+ * @param intval integer value
+ * @param ipos position
+ * @return integer expression
+ */
+klangc_expr_t *klangc_expr_new_int(int intval, klangc_ipos_t ipos);
+
+/**
+ * Create a new string expression.
+ * @param strval string value
+ * @param ipos position
+ * @return string expression
+ */
+klangc_expr_t *klangc_expr_new_string(const char *strval, klangc_ipos_t ipos);
+
+/**
+ * Create a new lambda expression.
+ * @param elambda lambda expression
+ * @param ipos position
+ * @return lambda expression
+ */
+klangc_expr_t *klangc_expr_new_lambda(klangc_expr_lambda_t *elambda,
+                                      klangc_ipos_t ipos);
+
+/**
+ * Create a new closure expression.
+ * @param eclosure closure expression
+ * @param ipos position
+ * @return closure expression
+ */
+klangc_expr_t *klangc_expr_new_closure(klangc_expr_closure_t *eclosure,
+                                       klangc_ipos_t ipos);
+
+// -------------------------------
+// Accessors.
+// -------------------------------
+/**
+ * Get the type of an expression.
+ * @param expr expression
+ * @return type
+ */
+klangc_expr_type_t klangc_expr_get_type(klangc_expr_t *expr);
+
+/**
+ * Get the algebraic expression of an expression.
+ * @param expr expression
+ * @return algebraic expression
+ */
+klangc_expr_alge_t *klangc_expr_get_alge(klangc_expr_t *expr);
+
+/**
+ * Get the reference of an expression.
+ * @param expr expression
+ * @return reference expression
+ */
+klangc_expr_ref_t *klangc_expr_get_ref(klangc_expr_t *expr);
+
+/**
+ * Get the application expression of an expression.
+ * @param expr expression
+ * @return application expression
+ */
+klangc_expr_appl_t *klangc_expr_get_appl(klangc_expr_t *expr);
+
+/**
+ * Get the integer value of an expression.
+ * @param expr expression
+ * @return integer value
+ */
+int klangc_expr_get_int(klangc_expr_t *expr);
+
+/**
+ * Get the string value of an expression.
+ * @param expr expression
+ * @return string value
+ */
+const char *klangc_expr_get_string(klangc_expr_t *expr);
+
+/**
+ * Get the lambda expression of an expression.
+ * @param expr expression
+ * @return lambda expression
+ */
+klangc_expr_lambda_t *klangc_expr_get_lambda(klangc_expr_t *expr);
+
+/**
+ * Get the closure expression of an expression.
+ * @param expr expression
+ * @return closure expression
+ */
+klangc_expr_closure_t *klangc_expr_get_closure(klangc_expr_t *expr);
+
+/**
+ * Get the position of an expression.
+ * @param expr expression
+ * @return position
+ */
+klangc_ipos_t klangc_expr_get_ipos(klangc_expr_t *expr);
+
+// -------------------------------
+// Parsers.
+// -------------------------------
+/**
+ * Parse an expression.
+ * @param input input
+ * @param epopt parse options
+ * @param pexpr parsed expression
+ * @return parse result
  */
 klangc_parse_result_t klangc_expr_parse(klangc_input_t *input,
                                         klangc_expr_parse_opt_t epopt,
                                         klangc_expr_t **pexpr);
 
+// -------------------------------
+// Printers.
+// -------------------------------
 /**
- * 式を出力する
- * @param output 出力
- * @param prec 優先順位
- * @param expr 式
+ * Print an expression.
+ * @param output output
+ * @param prec precedence
+ * @param expr expression
  */
 void klangc_expr_print(klangc_output_t *output, int prec, klangc_expr_t *expr);
 
+// -------------------------------
+// Binders.
+// -------------------------------
 /**
- * 式に含まれる参照を解決する
- * @param upper 上位環境
- * @param expr 式
- * @return 解決結果
+ * Bind an expression.
+ * @param env environment
+ * @param expr expression
+ * @return bind result
  */
-klangc_bind_result_t klangc_expr_bind(klangc_expr_env_t *upper,
+klangc_bind_result_t klangc_expr_bind(klangc_expr_env_t *env,
                                       klangc_expr_t *expr);
-
-/**
- * 式の位置情報を取得する
- * @param expr 式
- * @return 位置情報
- */
-klangc_ipos_t klangc_expr_get_ipos(klangc_expr_t *expr);
-
-/**
- * 式タイプを取得する
- * @param expr 式
- * @return 式タイプ
- */
-klangc_expr_type_t klangc_expr_get_type(klangc_expr_t *expr);
-
-/**
- * シンボル（式）を取得する
- * @param expr 式
- * @return シンボル（式）
- */
-klangc_symbol_t *klangc_expr_get_symbol(klangc_expr_t *expr);
-
-/**
- * 参照（式）を取得する
- * @param expr 式
- * @return 参照（式）
- */
-klangc_expr_ref_t *klangc_expr_get_ref(klangc_expr_t *expr);
-
-/**
- * 適用（式）を取得する
- * @param expr 式
- * @return 適用（式）
- */
-klangc_expr_appl_t *klangc_expr_get_appl(klangc_expr_t *expr);
-
-/**
- * 整数を取得する
- * @param expr 式
- * @return 整数
- */
-int klangc_expr_get_int(klangc_expr_t *expr);
-
-/**
- * 文字列を取得する
- * @param expr 式
- * @return 文字列
- */
-const char *klangc_expr_get_string(klangc_expr_t *expr);
-
-/**
- * ラムダ式（式）を取得する
- * @param expr 式
- * @return ラムダ式（式）
- */
-klangc_expr_lambda_t *klangc_expr_get_lambda(klangc_expr_t *expr);
-
-/**
- * クロージャ（式）を取得する
- * @param expr 式
- * @return クロージャ（式）
- */
-klangc_expr_closure_t *klangc_expr_get_closure(klangc_expr_t *expr);
