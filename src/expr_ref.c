c9e392016fc257e4b0bd5a994a6539172218460f
Makoto Katsumata <mako10k@mk10.org>
2024-08-13 11:07:48 +0900

    add comment for expr_ref

--- src/expr_ref.c
+++ src/expr_ref.c
@@ -6,38 +6,60 @@
 #include "types.h"
 #include <assert.h>
 
+// *******************************
+// Reference expression.
+// *******************************
+// -------------------------------
+// Structures.
+// -------------------------------
+/**
+ * Reference expression.
+ */
 struct klangc_expr_ref {
+  /** Reference */
   klangc_ref_t *ker_ref;
+  /** Target */
   klangc_expr_ref_target_t *ker_target;
 };
 
+// -------------------------------
+// Constructors.
+// -------------------------------
 klangc_expr_ref_t *klangc_expr_ref_new(klangc_ref_t *ref) {
   assert(ref != NULL);
-  klangc_expr_ref_t *expr = klangc_malloc(sizeof(klangc_expr_ref_t));
-  expr->ker_ref = ref;
-  expr->ker_target = NULL;
-  return expr;
+  klangc_expr_ref_t *eref = klangc_malloc(sizeof(klangc_expr_ref_t));
+  eref->ker_ref = ref;
+  eref->ker_target = NULL;
+  return eref;
 }
 
-klangc_ref_t *klangc_expr_ref_get_ref(klangc_expr_ref_t *expr) {
-  return expr->ker_ref;
-}
-
-klangc_expr_ref_target_t *klangc_expr_ref_get_target(klangc_expr_ref_t *expr) {
-  return expr->ker_target;
-}
-
-void klangc_expr_ref_set_target(klangc_expr_ref_t *expr,
+void klangc_expr_ref_set_target(klangc_expr_ref_t *eref,
                                 klangc_expr_ref_target_t *target) {
-  expr->ker_target = target;
+  assert(eref != NULL);
+  assert(target != NULL);
+  eref->ker_target = target;
 }
 
-klangc_ipos_t klangc_expr_ref_get_ipos(klangc_expr_ref_t *expr) {
-  return klangc_ref_get_ipos(expr->ker_ref);
+// -------------------------------
+// Accessors.
+// -------------------------------
+klangc_ref_t *klangc_expr_ref_get_ref(klangc_expr_ref_t *eref) {
+  assert(eref != NULL);
+  return eref->ker_ref;
 }
 
+klangc_expr_ref_target_t *klangc_expr_ref_get_target(klangc_expr_ref_t *eref) {
+  assert(eref != NULL);
+  return eref->ker_target;
+}
+
+// -------------------------------
+// Parsers.
+// -------------------------------
 klangc_parse_result_t klangc_expr_ref_parse(klangc_input_t *input,
-                                            klangc_expr_ref_t **pexpr_ref) {
+                                            klangc_expr_ref_t **eref) {
+  assert(input != NULL);
+  assert(eref != NULL);
   klangc_ref_t *ref;
   switch (klangc_ref_parse(input, &ref)) {
   case KLANGC_PARSE_OK:
@@ -47,19 +69,26 @@ klangc_parse_result_t klangc_expr_ref_parse(klangc_input_t *input,
   case KLANGC_PARSE_ERROR:
     return KLANGC_PARSE_ERROR;
   }
-  *pexpr_ref = klangc_expr_ref_new(ref);
+  *eref = klangc_expr_ref_new(ref);
   return KLANGC_PARSE_OK;
 }
 
-void klangc_expr_ref_print(klangc_output_t *output,
-                           klangc_expr_ref_t *expr_ref) {
-  klangc_ref_print(output, expr_ref->ker_ref);
+// -------------------------------
+// Printers.
+// -------------------------------
+void klangc_expr_ref_print(klangc_output_t *output, klangc_expr_ref_t *eref) {
+  assert(output != NULL);
+  assert(eref != NULL);
+  klangc_ref_print(output, eref->ker_ref);
 }
 
-klangc_bind_result_t klangc_expr_ref_bind(klangc_expr_env_t *env,
+// -------------------------------
+// Binders.
+// -------------------------------
+klangc_bind_result_t klangc_expr_ref_bind(klangc_expr_env_t *eenv,
                                           klangc_expr_ref_t *eref) {
   klangc_ref_t *ref = klangc_expr_ref_get_ref(eref);
-  klangc_expr_ref_target_t *target = klangc_expr_env_get_entry(env, ref);
+  klangc_expr_ref_target_t *target = klangc_expr_env_get_entry(eenv, ref);
   if (target == NULL) {
     klangc_ipos_t ipos = klangc_ref_get_ipos(ref);
     klangc_printf_ipos(kstderr, ipos, "W: unbound reference ");
--- src/expr_ref.h
+++ src/expr_ref.h
@@ -1,24 +1,69 @@
 #pragma once
 
 #include "klangc.h"
-#include "types.h"
 
+// *******************************
+// Reference expression.
+// *******************************
+// -------------------------------
+// Constructors.
+// -------------------------------
+/**
+ * Make a new reference expression.
+ * @param ref Reference.
+ * @return New reference expression.
+ */
 klangc_expr_ref_t *klangc_expr_ref_new(klangc_ref_t *ref);
 
-klangc_ref_t *klangc_expr_ref_get_ref(klangc_expr_ref_t *expr);
-
-klangc_ipos_t klangc_expr_ref_get_ipos(klangc_expr_ref_t *expr);
-
-klangc_expr_ref_target_t *klangc_expr_ref_get_target(klangc_expr_ref_t *expr);
-
-void klangc_expr_ref_set_target(klangc_expr_ref_t *expr,
+/**
+ * Set the target of a reference expression.
+ * @param eref Reference expression.
+ * @param target Target.
+ */
+void klangc_expr_ref_set_target(klangc_expr_ref_t *eref,
                                 klangc_expr_ref_target_t *target);
 
+// -------------------------------
+// Accessors.
+// -------------------------------
+/**
+ * Get the reference of a reference expression.
+ * @param eref Reference expression.
+ * @return Reference.
+ */
+klangc_ref_t *klangc_expr_ref_get_ref(klangc_expr_ref_t *eref);
+
+/**
+ * Get the target of a reference expression.
+ * @param eref Reference expression.
+ * @return Target.
+ */
+klangc_expr_ref_target_t *klangc_expr_ref_get_target(klangc_expr_ref_t *eref);
+
+// -------------------------------
+// Parsers.
+// -------------------------------
+/**
+ * Parse a reference expression.
+ * @param input Input stream.
+ * @param eref Pointer to the result.
+ * @return Parsing result.
+ */
 klangc_parse_result_t klangc_expr_ref_parse(klangc_input_t *input,
-                                            klangc_expr_ref_t **pexpr_ref);
+                                            klangc_expr_ref_t **peref);
 
-void klangc_expr_ref_print(klangc_output_t *output,
-                           klangc_expr_ref_t *expr_ref);
+// -------------------------------
+// Printers.
+// -------------------------------
+/**
+ * Print a reference expression.
+ * @param output Output stream.
+ * @param eref Reference expression.
+ */
+void klangc_expr_ref_print(klangc_output_t *output, klangc_expr_ref_t *eref);
 
-klangc_bind_result_t klangc_expr_ref_bind(klangc_expr_env_t *env,
-                                          klangc_expr_ref_t *expr);
+// -------------------------------
+// Binders.
+// -------------------------------
+klangc_bind_result_t klangc_expr_ref_bind(klangc_expr_env_t *eenv,
+                                          klangc_expr_ref_t *eref);
